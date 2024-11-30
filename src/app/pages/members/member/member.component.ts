821de7a882c42bd48870f3ba95cd8e47ab9a79cc
mofirojean <mofirojean@gmail.com>
2024-11-30 12:19:26 +0100

    feat - add api for avatar

--- src/app/pages/members/member/member.component.ts
+++ src/app/pages/members/member/member.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faBrandIcon } from '../../../utils/icon.utils';
+import { GithubService } from '../../../service/github/github.service';
+import { Member } from '../../../members/members';
 
 @Component({
   selector: 'ngc-member',
@@ -8,6 +10,18 @@ import { faBrandIcon } from '../../../utils/icon.utils';
   templateUrl: './member.component.html',
   styles: ``
 })
-export class MemberComponent {
+export class MemberComponent implements OnInit {
   protected readonly faBrandIcon = faBrandIcon;
+  @Input() member!: Member
+  protected avatarUrl = ""
+
+  constructor(
+    private githubService: GithubService
+  ) {}
+
+  ngOnInit(): void {
+    this.githubService.getGithubProfile(this.member.socials.github).subscribe(res => {
+      this.avatarUrl = res.avatar_url
+    })
+  }
 }
